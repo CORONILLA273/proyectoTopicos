f86d6df46d0ec11333576fa1f4ad39d5cf5c0bc2
Coronilla <e.coronillahernandez@ugto.com>
2024-11-08 00:11:18 -0600

    Lista de ejercicios

---
+++ SmarTrainer/app/index.jsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import exercises from '../assets/data/exercises.json'
+import ExerciseListItem from '../components/ExerciseListItem'
+
+// Define el tipo de cada ejercicio
+
+
+
+export default function App () {
+
+    return (
+        <View style={ styles.container }>
+            <FlatList
+                data = {exercises}
+                contentContainerStyle = {{gap:5}}
+                keyExtractor={(item, index) => item.name + index}
+                renderItem = {({ item }) => <ExerciseListItem item= {item} />}
+            />
+            
+            <StatusBar style='auto'/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'gainsboro',
+        justifyContent: 'center',
+        padding: 10,
+        paddingTop: 10,
+    }
+})
